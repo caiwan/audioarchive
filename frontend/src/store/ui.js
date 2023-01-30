cc74a3064b233b2f0f1d027f903b1291b235edc9
Caiwan <caiwan.lupus@gmail.com>
2023-01-30 13:17:48 +0100

    Add frontend

---
+++ frontend/src/store/ui.js
@@ -0,0 +1,48 @@
+// export default {
+//     namespaced: true,
+//     state:{},
+//     getters:{},
+//     actions:{},
+//     mutations:{}
+//   };
+
+const SNACKBAR_TIMEOUT_MSECONDS = 5000
+
+export default {
+    namespaced: true,
+    state: {
+        showSidebar: false,
+        showSnackbar: false,
+        showUserMenu: false,
+        snackbarMessages: [],
+        snackbarTimeout: null,
+        loading: 0
+    },
+    getters: {
+        isLoading(state, getters, rootState) { return !!state.loading || rootState.App.isInitializing; }
+    },
+
+    mutations: {
+        toggle: (state, property) => { state[property] = !state[property]; },
+        pushSnackbar(state, message) {
+            state.snackbarMessages.push(message);
+            state.showSnackbar = true;
+        },
+        pullSnackbar(state) {
+            state.snackbarMessages = [];
+            state.showSnackbar = false;
+            state.snackbarTimeout = null;
+        }
+    },
+
+    actions: {
+        pushIOError({ state, commit }, error) {
+            console.error('snackbar', error);
+            commit('pushSnackbar', `${error}`);
+            if (state.snackbarTimeout) clearTimeout(state.snackbarTimeout);
+            state.snackbarTimeout = setTimeout(() => { commit('pullSnackbar'); }, SNACKBAR_TIMEOUT_MSECONDS);
+        },
+        pushLoad({ state }) { ++state.loading; },
+        popLoad({ state }) { --state.loading; if (state.loading < 0) { console.error('loading queue underflows'); state.loading = 0; } }
+    }
+};
